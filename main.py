80e1e67d0666e8fdbcada2b388b0910d0cbee89d
PranayPandare <101919618+PranayPandare@users.noreply.github.com>
2022-03-31 09:38:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,98 +1,162 @@
-from tkinter import*
-import tkinter
-import qrcode
-import  sqlite3
-from PIL import Image,ImageTk
-from resizeimage import resizeimage
-class Qr_Generator:
-    def __init__(self, root ):
-        self.root=root
-        self.root.geometry("900x500+200+50")
-        self.root.title("QR_Generator")
-        self.root.resizable(False,False)
+# Python program for jumbled words game.
 
-        title = Label(self.root,text="  Qr Code Generator",font=("times new roman",40),bg='#053246',fg='white', anchor="w").place(x=0, y=0,relwidth= 1)
-
-        #=======Employee Details window========
-        #=====variables=======
-        self.var_emp_code=StringVar()
-        self.var_name=StringVar()
-        self.var_department=StringVar()
-        self.var_designation=StringVar()
-        emp_Frame=Frame(self.root,bd=2,relief=RIDGE,bg='white')
-        emp_Frame.place(x=50,y=100,width=500,height=380)
-
-        emp_title = Label(emp_Frame, text="Employee Details", font=("goudy old style", 20), bg='#043256', fg='white').place(x=0, y=0, relwidth=1)
-
-        lbl_emp_code=Label(emp_Frame,text="Employee ID",font=("times new roman",15,'bold'),bg='white').place(x=20,y=60)
-        lbl_name=Label(emp_Frame,text="Name",font=("times new roman",15,'bold'),bg='white').place(x=20,y=100)
-        lbl_department=Label(emp_Frame,text="Department",font=("times new roman",15,'bold'),bg='white').place(x=20,y=140)
-        lbl_emp_designation=Label(emp_Frame,text="Designation",font=("times new roman",15,'bold'),bg='white').place(x=20,y=180)
-
-        txt_emp_code=Entry(emp_Frame,font=("times new roman",15),textvariable=self.var_emp_code,bg='lightyellow').place(x=200,y=60)
-        txt_name=Entry(emp_Frame,font=("times new roman",15),textvariable=self.var_name,bg='lightyellow').place(x=200,y=100)
-        txt_department=Entry(emp_Frame,font=("times new roman",15),textvariable=self.var_department,bg='lightyellow').place(x=200,y=140)
-        txt_emp_designation=Entry(emp_Frame,font=("times new roman",15),textvariable=self.var_designation,bg='lightyellow').place(x=200,y=180)
-
-        btn_generate=Button(emp_Frame,text='QR Generate',command=self.generate,font=("times new roman",18,'bold'),bg='#2196f3',fg='white').place(x=90,y=250,width=180,height=30)
-        btn_clear=Button(emp_Frame,text='Clear',command=self.clear,font=("times new roman",18,'bold'),bg='#607d8b',fg='white').place(x=282,y=250,width=120,height=30)
+# import random module
+import random
 
 
-        self.msg=''
-        self.lbl_msg=Label(emp_Frame,text=self.msg,font=("times new roman",20),bg='white',fg='green')
-        self.lbl_msg.place(x=0,y=310,relwidth=1)
+# function for choosing random word.
+def choose():
+	# list of word
+	words = ['rainbow', 'computer', 'science', 'programming',
+			'mathematics', 'player', 'condition', 'reverse',
+			'water', 'board', 'geeks']
 
-        # =======Employee QR code window========
-        qr_Frame = Frame(self.root, bd=2, relief=RIDGE, bg='white')
-        qr_Frame.place(x=600, y=100, width=250, height=380)
+	# choice() method randomly choose
+	# any word from the list.
+	pick = random.choice(words)
 
-        emp_title = Label(qr_Frame, text="Employee QR code", font=("goudy old style", 20), bg='#043256',fg='white').place(x=0, y=0, relwidth=1)
-
-        self.qr_code=Label(qr_Frame,text='No Qr\nAvailable',font=('times new roman',15),bg='#3f51b5',fg='white',bd=1,relief=RIDGE)
-        self.qr_code.place(x=35,y=100,width=180,height=180)
-
-
-    def clear(self):
-        self.var_emp_code.set('')
-        self.var_name.set('')
-        self.var_department.set('')
-        self.var_designation.set('')
-        self.msg = ''
-        self.lbl_msg.config(text=self.msg)
-        self.qr_code.config(image='')
-
-    def generate(self):
-       if self.var_designation.get()=='' or self.var_emp_code.get()=='' or self.var_department.get()=='' or self.var_name.get()=='':
-            self.msg ='All Fields are Required!!!'
-            self.lbl_msg.config(text=self.msg, fg='red')
-       else:
-            qr_data=(f"Employee ID: {self.var_emp_code.get()}\nEmployee Name:{self.var_name.get()}\nDepartment: {self.var_department.get()}\nDesgination: {self.var_designation.get()}")
-            qr_code=qrcode.make(qr_data)
-            #print(qr_code
-            qr_code=resizeimage.resize_cover(qr_code,[180,180])
-            qr_code.save("employee_QR/Emp_"+str(self.var_emp_code.get())+'.png')
-            #======QR code image update========
-            self.im=ImageTk.PhotoImage(file="employee_QR/Emp_"+str(self.var_emp_code.get())+'.png')
-            self.qr_code.config(image=self.im)
-            #=======updating  notification====
-            self.msg='QR Generated Successfully!!!'
-            self.lbl_msg.config(text=self.msg,fg='green')
-            self.save_details()
-    def save_details(self):
-        conn = sqlite3.connect('Details.db')
-        c = conn.cursor()
-
-        c.execute(
-            "CREATE TABLE Details1 (Employee_Id integer, Name text, Department text, Designation text);")
-
-        c.execute("INSERT INTO Details1 (Employee_Id,Name, Department,Designation) VALUES(?,?,?,?)",
-                  (str(self.var_emp_code.get()), str(self.var_name.get()), str(self.var_department.get()), str(self.var_designation.get())))
-
-        conn.commit()
-        print("data entered")
-        conn.close()
+	return pick
 
 
-root=Tk()
-obj =Qr_Generator(root)
-root.mainloop()
+# Function for shuffling the
+# characters of the chosen word.
+def jumble(word):
+	# sample() method shuffling the characters of the word
+	random_word = random.sample(word, len(word))
+
+	# join() method join the elements
+	# of the iterator(e.g. list) with particular character .
+	jumbled = ''.join(random_word)
+	return jumbled
+
+
+# Function for showing final score.
+def thank(p1n, p2n, p1, p2):
+	print(p1n, 'Your score is :', p1)
+	print(p2n, 'Your score is :', p2)
+
+	# check_win() function calling
+	check_win(p1n, p2n, p1, p2)
+
+	print('Thanks for playing...')
+
+
+# Function for declaring winner
+def check_win(player1, player2, p1score, p2score):
+	if p1score > p2score:
+		print("winner is :", player1)
+	elif p2score > p1score:
+		print("winner is :", player2)
+	else:
+		print("Draw..Well Played guys..")
+
+
+# Function for playing the game.
+def play():
+	# enter player1 and player2 name
+	p1name = input("player 1, Please enter your name :")
+	p2name = input("Player 2 , Please enter your name: ")
+
+	# variable for counting score.
+	pp1 = 0
+	pp2 = 0
+
+	# variable for counting turn
+	turn = 0
+
+	# keep looping
+	while True:
+
+		# choose() function calling
+		picked_word = choose()
+
+		# jumble() function calling
+		qn = jumble(picked_word)
+		print("jumbled word is :", qn)
+
+		# checking turn is odd or even
+		if turn % 2 == 0:
+
+			# if turn no. is even
+			# player1 turn
+			print(p1name, 'Your Turn.')
+
+			ans = input("what is in your mind? ")
+
+			# checking ans is equal to picked_word or not
+			if ans == picked_word:
+
+				# incremented by 1
+				pp1 += 1
+
+				print('Your score is :', pp1)
+				turn += 1
+
+			else:
+				print("Better luck next time ..")
+
+				# player 2 turn
+				print(p2name, 'Your turn.')
+
+				ans = input('what is in your mind? ')
+
+				if ans == picked_word:
+					pp2 += 1
+					print("Your Score is :", pp2)
+
+				else:
+					print("Better luck next time...correct word is :", picked_word)
+
+				c = int(input("press 1 to continue and 0 to quit :"))
+
+				# checking the c is equal to 0 or not
+				# if c is equal to 0 then break out
+				# of the while loop o/w keep looping.
+				if c == 0:
+					# thank() function calling
+					thank(p1name, p2name, pp1, pp2)
+					break
+
+		else:
+
+			# if turn no. is odd
+			# player2 turn
+			print(p2name, 'Your turn.')
+			ans = input('what is in your mind? ')
+
+			if ans == picked_word:
+				pp2 += 1
+				print("Your Score is :", pp2)
+				turn += 1
+
+			else:
+				print("Better luck next time.. :")
+				print(p1name, 'Your turn.')
+				ans = input('what is in your mind? ')
+
+				if ans == picked_word:
+					pp1 += 1
+					print("Your Score is :", pp1)
+
+				else:
+					print("Better luck next time...correct word is :", picked_word)
+
+					c = int(input("press 1 to continue and 0 to quit :"))
+
+					if c == 0:
+						# thank() function calling
+						thank(p1name, p2name, pp1, pp2)
+						break
+
+			c = int(input("press 1 to continue and 0 to quit :"))
+			if c == 0:
+				# thank() function calling
+				thank(p1name, p2name, pp1, pp2)
+				break
+
+
+# Driver code
+if __name__ == '__main__':
+	
+	# play() function calling
+	play()
